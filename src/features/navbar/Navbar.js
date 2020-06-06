950fd24687c46fe5ee41ec5bf10b39a14acefa55
Begum Ozcan <begumozcan95@gmail.com>
2020-06-06 23:21:05 +0200

    Add signature and README.md

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -5,8 +5,13 @@ import logo from '../../pokeball.png';
 function Navbar() {
     return (
         <div className="Navbar">
-            <img alt="logo" src={logo} width="48px"/>
-            <span className="title">PokéMessenger</span>
+            <div className="branding">
+                <img alt="logo" src={logo} width="48px"/>
+                <span className="title">PokéMessenger</span>
+            </div>
+            <div className="signature">
+                made with &#10084; by <a href="http://www.github.com/begozcan" target="_blank">begozcan</a>
+            </div>
         </div>
     );
 }
